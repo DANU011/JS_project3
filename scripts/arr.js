5c0e3102798d9bdac5d699401ad6685935adcd6d
DANU011 <qnfrdms2003@gmail.com>
2023-01-25 12:13:13 +0900

    v2.0

--- scripts/arr.js
+++ scripts/arr.js
@@ -1,29 +1,61 @@
-/* 배열 선언 */
- 
+/* 배열 선언 /배열 = 위치를 가지고 여러개의 데이터를 구분,위치가 키가 됨, 데이터 타입 달라도 가능 vs obj */
+let arr = []; /*추천! */
+let arr2= new Array(); /*생성자에 의해 인스턴스만 하나 생성*/ 
 
-/* 배열 추가 삭제 :뒤에서 */
+/* 배열 추가 삭제 :뒤에서*/
+arr.push('🍕');
+arr.push('🍔');
+arr.push('🍟');
+console.log(arr);
+
+arr.pop();
+console.log(arr);
  
-/* 배열 추가 삭제 :앞에서 */
-  
+/* 배열 추가 삭제 :앞에서/ 비추 */
+arr.unshift('😀');
+arr.unshift('🤣') ;
+console.log(arr); 
+
+arr.shift('😍');
+console.log(arr); 
+
 
 /* 배열 순회 */
+for(let i = 0; i<arr.length; i++){
+console.log(arr[i]);
+}
+
+for(let i of arr){
+console.log(i);
+}
  
 /* 배열의 length 속성*/
- 
+console.log(arr);
+console.log(arr.length);
 
 //배열 비우기
- 
+arr.length = 0;
+console.log(arr); 
 
-/* 배열을 문자열로 반환*/
+/* 배열을 문자열로 반환/ 요즘음 변경이 될 가능성이 많으면 배열 마지막에 ,를 넣어도 오류X*/
+/*window + . 이모지 */
 arr = ['🍕','🍔','🍗','🍜',] ;
+let s = String(arr)
+console.log(arr)
  
-/* 문자열 split하면 배열반환*/
+/* 문자열 split하면 배열반환 / 많이 씀*/
+let sarr = s.split(',')
+console.log(sarr)
  
-/* 배열 정렬 */
+/* 배열 정렬 - 콜백함수 이용 a-b 오름차순*/
 let num = [1,3,5,2,4];
+num.sort((a,b)=>{return b-a;});
+console.log(num);
  
 
 /* 배열 섞기*/
+num.sort(()=> Math.random() - 0.5)
+console.log(num);
  
  
 
@@ -35,16 +67,23 @@ let setA = [1,2,3,4,5];
 let setB = [4,5,6,7,8];
 
 //합집합
-let union =  
+let union = [...setA, ...setB] 
 console.log(union);
 
 //교집합
-let intersection =  
+let intersection = setA.filter((item)=>setB.includes(item)) 
 console.log(intersection);
 
+let setA2 = setA.map((item)=>item%2)
+console.log(setA2);
+
 //차집합
-let difference =  
+let difference =setA.filter((item)=>!setB.includes(item))
 console.log(difference);
+
+//합집합 수정
+union = [...difference, ...setB] 
+console.log(union);
 
 
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,99 @@
 
 //폭탄이 있는 위치를 나타내는 배열
 //DOM이 로드가 되면 반복문을 이용하여 [0,0,0,0,0,0,0,0,1]로 초기화
-let num = [0,0,0,0,0,0,0,0,1];
+let num = [0,0,0,0,0,0,0,0,1];//폭탄 위치를 가지고 있음
 
 //박스를 선택한 순서를 기록하는 배열
-let selNum = [];
+let selNum = [];//몇번째를 눌렀는지 추적하기 위함
 
 //박스를 선택한 개수를 기록하는 변수
-let cnt = 0 ;
+let cnt = 0 ;//폭탄이 안나올 때 까지 몇번 눌렸는지 화인
 
 //폭탄이 섞였는지 체크하는 flag변수
 let shuffleFlag = false;
 
+//메세지 표시 함수
+const showMsg = (m) =>{
+     document.querySelector("#msg").innerHTML=m;
+}
+
+//보드 내용 지우기
+const boardInit = () => {
+     const board = document.querySelectorAll(".boardBox");
+     for(let item of board){
+          item.innerHTML='';
+     }
+     cnt=0; //초기화
+     selNum.length = 0; //초기화
+     console.log(board);
+}
+
+//전체 하트 만들기
+const showAll=()=>{
+     const board = document.querySelectorAll(".boardBox");
+     for(let item of board){
+          item.innerHTML = `<img src="./images/hart.png">`;
+     }
+     /*let idx = num.indexOf(1) +1;
+     console.log('last: ', idx);
+     document.querySelector("#box"+idx).innerHTML =
+                    `<img src="./images/hart.png">`;*/
+
+
+     
+
+}
+//보드 숫자 누른 후 보여주기 함수
+const show = (n) => {
+     if(!shuffleFlag) {
+          showMsg('게임 종료. 다시 하세요.');
+          //boxShuffle();
+          return;
+     }
+     console.log(n);
+
+
+     cnt++;//누른 횟수
+     selNum.push(n);//눌러진 정보 저장
+     console.log(selNum);
+     
+
+     let img;
+     if(num[n-1]==0) img = 'hart'
+     else {
+          img= 'boom';
+          shuffleFlag = false;
+          showMsg('게임 실패!!');
+     }
+
+     document.querySelector("#box"+n).innerHTML =`<img src="./images/${img}.png">`;
+
+     if(cnt==8){
+          showAll();
+          shuffleFlag = false;
+          showMsg('게임 성공!!');
+          return;
+     }
+
+
+}
+
+//폭탄 섞기 함수
+const boxShuffle=()=>{ 
+     if(shuffleFlag) {
+          showMsg('폭탄을 이미 섞었습니다. 게임을 진행해주세요.');
+          boxShuffle();
+          return;
+          }  
+     num.sort(()=>Math.random()-0.5);
+     shuffleFlag = true;
+     boardInit();
+     console.log(num);
+  
+     } 
+     
+          
+     
 
 /* DOM이 로드된 후에 클릭이벤트 연결*/
 document.addEventListener("DOMContentLoaded", ()=>{
